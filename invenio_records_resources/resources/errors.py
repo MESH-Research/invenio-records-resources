3a0c79b3b077d134c4c0380ef970a304e57b157e
Ian Scott <scottianw@gmail.com>
2023-12-14 13:43:15 -0500

    Revert "files: abort with 500 on upload failures"
    This reverts commit f0db4067e89148ce122066258f765f48e59ea4b5.

--- invenio_records_resources/resources/errors.py
+++ invenio_records_resources/resources/errors.py
@@ -33,7 +33,6 @@ from werkzeug.routing import BuildError
 from ..errors import validation_error_to_list_errors
 from ..services.errors import (
     FacetNotFoundError,
-    FailedFileUploadException,
     FileKeyNotFoundError,
     PermissionDeniedError,
     QuerystringValidationError,
@@ -186,11 +185,5 @@ class ErrorHandlersMixin:
         ),
         search.exceptions.RequestError: create_error_handler(
             lambda e: HTTPJSONSearchRequestError(e)
-        ),
-        FailedFileUploadException: create_error_handler(
-            HTTPJSONException(
-                code=400,
-                description="The file upload failed, please try again",
-            )
         ),
     }
